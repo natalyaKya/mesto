b949e3ffbe5c2a929e5ab1d21661c2f46d438b1a
NatalyaKya <natalya_kya@mail.ru>
2023-02-19 15:40:53 +1000

    commit1

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let clickEditButton = document.querySelector('.profile__edit-button');
+let form = document.querySelector('.form');
+
+let nameInput = document.querySelector('.form__name');
+let jobInput = document.querySelector('.form__job');
+
+let profileName = document.querySelector('.profile__heading');
+let profileJob = document.querySelector('.profile__text');
+
+
+clickEditButton.addEventListener('click', function(){
+    form.classList.add('form_opened');
+
+})
+
+let closeForm = document.querySelector('.form__close-icon');
+
+closeForm.addEventListener('click', function(){
+    form.classList.remove('form_opened');
+})
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+
+    form.classList.remove('form_opened');
+}
+
+form.addEventListener('submit', handleFormSubmit);
